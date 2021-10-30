69e8edce1f628f67b478acdd5fd897b01aa1ecdf
himanshu udupi <himanshuudupi365@outlook.com>
2021-10-30 16:25:51 +0530

    hills 2*2 decryption testing

--- hills.py
+++ hills.py
@@ -13,7 +13,7 @@ def alphabetInitialise():
 def keyGenerator():
     keyList = []
     for a in range(0,4):
-        randomNum = random.randint(11,43)
+        randomNum = random.randint(1,20)
         keyList.append(randomNum)
     matrix = np.reshape(keyList , (2,2))
     return matrix 
@@ -31,9 +31,30 @@ def encryption(plainText):
     for b in range(0, rowLength):
         multipliedMatrix = index[b]
         multipliedMatrix = multipliedMatrix@matrixInv
-        cipherChr.extend([alphabet[multipliedMatrix[0]%26] , alphabet[multipliedMatrix[1]%26]])
+        cipherChr.extend([alphabet[multipliedMatrix[0]%27] , alphabet[multipliedMatrix[1]%27]])
     cipher = "".join(cipherChr)
-    print(keyMatrix,cipher, sep="\n")
+    print([cipher,matrixInv], sep="\n")
 
 
-encryption("HELLO WORLD")
+# encryption("HELLO WORLD")
+
+
+def decryption(cipher):
+    keyMatrix = np.array(cipher[1])
+    alphabet = alphabetInitialise()
+    indexes = np.reshape(list(map(lambda x : alphabet.index(x) , list(cipher[0]))), (len(cipher[0])//2,2))
+    matrixMax = []
+    for a in range(0,2):
+        matrixSum = sum(keyMatrix[:,a])
+        matrixMax.append(matrixSum)
+    print(matrixMax , keyMatrix , indexes, sep="\n")
+    constTerm = (keyMatrix[0,0]*keyMatrix[1,1] - keyMatrix[0,1]*keyMatrix[1,0])
+    possibleValues = []
+    counter = 0
+    # while counter < len(cipher)//2:
+    #     remainderList = indexes[counter]
+    #     individualPossibleValues = []
+    #     # for a in range(0, (matrixMax[0])):
+
+
+decryption(['RLKKHEFQWKVN', [[11,  9],[12, 14]]])
